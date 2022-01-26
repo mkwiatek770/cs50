b8faf67b999dac7003a358ae376420ca93d22307
Michal Kwiatek <michalakwiatek@gmail.com>
2022-01-26 17:00:22 +0100

    fix problem with \n char in catched input name

--- lecture_2_c/hello/hello.c
+++ lecture_2_c/hello/hello.c
@@ -1,10 +1,14 @@
 #include <stdio.h>
+#include <string.h>
+
 
 int main(void){
 
     char name[20];
     printf("What's your name? ");
     fgets(name, 20, stdin);
-    printf("Hello, %s!", name);
+    // clear \n char from name
+    name[strlen(name)-1] = '\0';
+    printf("Hello, %s!\n", name);
     return 0;
 }
