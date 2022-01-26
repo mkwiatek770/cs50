fcac676c33f28855aabab605680c3cedb233d3a4
Michal Kwiatek <michalakwiatek@gmail.com>
2022-01-26 18:18:22 +0100

    functions and different type of loops

--- lecture_2_c/loops.c
+++ lecture_2_c/loops.c
@@ -1,13 +1,32 @@
 #include <stdio.h>
 
+void meow(void){
+    printf("meow\n");
+}
+
+void bark(void){
+    printf("woof\n");
+}
+
+void fsoc(void){
+    printf("fsociety\n");
+}
+
 
 int main(void){
     
     int i = 0;
     while (i++ < 3)
     {
-        printf("meow\n");
+        meow();
     }
     
+    for(int i = 0; i < 3; ++i){
+        bark();
+    }
+
+    do{
+        fsoc();
+    } while(1);
 
 }
