192028e3e6828d6b716378f4f77c8bf32db8ece8
Michal Kwiatek <michalakwiatek@gmail.com>
2022-01-27 16:39:50 +0100

    credit ps1 - initial thoughts

---
+++ pset_1/credit.c
@@ -0,0 +1,20 @@
+#include <cs50.h>
+#include <math.h>
+#include <stdio.h>
+
+const long int MIN_VALID_VISA = pow(10, 13);
+const long int MAX_VALID_VISA = pow(10, 16);
+const long int MAX_VALID_MASTERCARD = pow(10, 16);
+const long int MAX_VALID_MASTERCARD = pow(10, 16);
+const long int MAX_VALID_AEXPRESS = 34 * pow(10, 15);
+const long int MAX_VALID_AEXPRESS = 37 * pow(10, 15);
+
+
+
+
+int main(void)
+{
+    long int number = get_long("Number: ");
+
+
+}
