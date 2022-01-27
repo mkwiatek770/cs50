9dfbcb6b10d24d15f9cc67c3b591ed536eade230
Michal Kwiatek <michalakwiatek@gmail.com>
2022-01-27 10:21:54 +0100

    nested for loops

--- lecture_2_c/validating_input.c
+++ lecture_2_c/validating_input.c
@@ -8,4 +8,11 @@ int main(void){
     {
         n = get_int("Size: ");
     } while (n < 0);
+
+    for (int i = 0; i < n; i++){
+        for(int j = 0; j < n; j++){
+            printf("#");
+        }
+        printf("\n");
+    }
 }
