bd4116bac520456936bcbee7347740e8c9ef662f
Michal Kwiatek <michalakwiatek@gmail.com>
2022-01-27 16:19:02 +0100

    working drawing

--- pset_1/mario.c
+++ pset_1/mario.c
@@ -1,20 +1,35 @@
 #include <cs50.h>
 #include <stdio.h>
 
-
-int main(void){
+int main(void)
+{
     int h;
-    do{
+    int blank_w;
+    int hash_w;
+
+    do
+    {
         h = get_int("Size: ");
     } while (h < 1 && h > 8);
 
-    // rows
-    for (int i=0; i<h; i++){
-        for (int j=0; j<h; j++){
+    for (int i = 1; i <= h; i++)
+    {
+        blank_w = h - i;
+        hash_w = i;
+        // 1st half
+        for (int j = 0; j < blank_w; j++)
+        {
+            printf(" ");
+        }
+        for (int j = 0; j < hash_w; j++)
+        {
             printf("#");
         }
+        // middle gap
         printf(" ");
-        for (int j=0; j<h; j++){
+        // 2nd half
+        for (int j = 0; j < hash_w; j++)
+        {
             printf("#");
         }
         printf("\n");
