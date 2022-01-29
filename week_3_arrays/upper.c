845a38781f35d70667edecff7e78d160800e136c
Michal Kwiatek <michalakwiatek@gmail.com>
2022-01-29 22:27:17 +0100

    even better upper.c

--- week_3_arrays/upper.c
+++ week_3_arrays/upper.c
@@ -8,6 +8,7 @@ int main(void) {
     string name = get_string("String: ");
     printf("before: %s\n", name);
     printf("after:  ");
+    // 1st version
     // for (int i = 0, l = strlen(name); i < l; i++){
     //     if (name[i] >= 'a' && name[i] <= 'z'){
     //         printf("%c", name[i] - 32);
@@ -16,13 +17,18 @@ int main(void) {
     //         printf("%c", name[i]);
     //     }
     // }
+    // 2nd version
+    // for (int i = 0, l = strlen(name); i < l; i++){
+    //     if (islower(name[i])){
+    //         printf("%c", toupper(name[i]));
+    //     }
+    //     else{
+    //         printf("%c", name[i]);
+    //     }
+    // }
+    // 3rd version
     for (int i = 0, l = strlen(name); i < l; i++){
-        if (islower(name[i])){
-            printf("%c", toupper(name[i]));
-        }
-        else{
-            printf("%c", name[i]);
-        }
+        printf("%c", toupper(name[i]));
     }
     printf("\n");
 
