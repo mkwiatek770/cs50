1ca649d936c278a6e18a39f22dbc290d273482ca
Michal Kwiatek <michalakwiatek@gmail.com>
2022-02-10 16:01:12 +0100

    validation script

---
+++ pset_7/movies/test.sh
@@ -0,0 +1,16 @@
+#! /bin/bash
+
+declare -a expected;
+expected=(0 9952 1 69705 1 11 1 7046 4 18730 2236 5 6 185);
+
+
+for i in {1..13}; do
+    echo "Running $i.sql";
+    cat "$i.sql" | sqlite3 movies.db > "output$i.txt";
+    lines=`wc -l < "output$i.txt"`;
+    if [[ lines -eq ${expected[i]} ]]; then
+        echo "Passes";
+    else
+        echo "Fails";
+    fi
+done
